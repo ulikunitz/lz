773685d0e61f11608c5f56c9b56d449f8d392b57
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2021-06-07 08:20:49 +0200

    Add xbitset
    This was the first try of creating an extendable bitset. I plan to
    implement another versions which has a starting offset of for the
    extandable bitmap field.

--- bitset.go
+++ bitset.go
@@ -1,6 +1,10 @@
 package lz
 
-import "math/bits"
+import (
+	"fmt"
+	"math/bits"
+	"strings"
+)
 
 const bsMask = 1<<6 - 1
 
@@ -78,4 +82,146 @@ func (b *bitset) pop() int {
 		n += bits.OnesCount64(x)
 	}
 	return n
+}
+
+type xset []uint64
+
+func (s *xset) clear() *xset {
+	*s = (*s)[:0]
+	return s
+}
+
+func (s *xset) ensureLen(k int) *xset {
+	a := *s
+	if k <= len(a) {
+		return s
+	}
+	if k <= cap(a) {
+		n := len(a)
+		a = a[:k]
+		b := a[n:]
+		for i := range b {
+			b[i] = 0
+		}
+		*s = a
+		return s
+	}
+	b := make([]uint64, k)
+	copy(b, a)
+	*s = b
+	return s
+}
+
+func (s *xset) insert(i ...int) *xset {
+	a := *s
+	for _, j := range i {
+		k := j >> 6
+		if k >= len(a) {
+			a = *(s.ensureLen(k + 1))
+		}
+		a[k] |= 1 << uint(j&bsMask)
+	}
+	return s
+}
+
+func (s *xset) isMember(i int) bool {
+	a := *s
+	k := i >> 6
+	if k >= len(a) {
+		return false
+	}
+	return (a[k] & (1 << uint(i&bsMask))) != 0
+}
+
+func (s *xset) memberAfter(i int) (k int, ok bool) {
+	a := *s
+	i++
+	j := i >> 6
+	if j >= len(a) {
+		return 0, false
+	}
+	m := ^(uint64(1)<<uint(i&bsMask) - 1)
+	k = bits.TrailingZeros64(a[j] & m)
+	for {
+		if k < 64 {
+			return j<<6 + k, true
+		}
+		j++
+		if j >= len(a) {
+			return 0, false
+		}
+		k = bits.TrailingZeros64(a[j])
+	}
+}
+
+func (s *xset) firstMember() (k int, ok bool) {
+	a := *s
+	for j := 0; j < len(a); j++ {
+		k = bits.TrailingZeros64(a[j])
+		if k < 64 {
+			return j<<6 + k, true
+		}
+	}
+	return 0, false
+}
+
+func (s *xset) assign(u *xset) *xset {
+	if s == u {
+		return s
+	}
+	s.ensureLen(len(*u))
+	*s = (*s)[:len(*u)]
+	copy(*s, *u)
+	return s
+}
+
+func (s *xset) intersect(u, v *xset) *xset {
+	s.assign(u)
+	if u == v {
+		return s
+	}
+	a, b := *s, *v
+	n := len(a)
+	if len(b) < n {
+		n = len(b)
+	}
+	for i, v := range b[:n] {
+		a[i] &= v
+	}
+	for n--; n >= 0 && a[n] == 0; n-- {
+	}
+	n++
+	*s = a[:n]
+	return s
+}
+
+func (s *xset) slice() []int {
+	var a []int
+	for i, x := range *s {
+		j := i << 6
+		for {
+			k := bits.TrailingZeros64(x)
+			if k >= 64 {
+				break
+			}
+			j += k
+			a = append(a, j)
+			x >>= k + 1
+			j++
+		}
+	}
+	return a
+}
+
+func (s *xset) String() string {
+	var sb strings.Builder
+	sb.WriteByte('{')
+	for i, k := range s.slice() {
+		if i > 0 {
+			sb.WriteString(", ")
+		}
+		fmt.Fprint(&sb, k)
+	}
+	sb.WriteByte('}')
+	return sb.String()
 }
--- bitset_test.go
+++ bitset_test.go
@@ -1,6 +1,8 @@
 package lz
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestBitsetSimple(t *testing.T) {
 	var b bitset
@@ -72,5 +74,82 @@ func TestBitsetSimple(t *testing.T) {
 	}
 	if k != 127 {
 		t.Fatalf("b.memberAfter(%d) returns %d; want %d", 0, k, 127)
+	}
+}
+
+func equalIntSlices(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, x := range a {
+		if x != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestXSetSimple(t *testing.T) {
+	var a xset
+	if a.isMember(64) {
+		t.Fatalf("a.isMember(%d) returns true; want false", 64)
+	}
+	_, ok := a.firstMember()
+	if ok {
+		t.Fatalf("a.firstMember returns true; want false")
+	}
+	a.insert(64)
+	if !a.isMember(64) {
+		t.Fatalf("a.isMember(%d) returns false; want true", 64)
+	}
+	var i int
+	i, ok = a.firstMember()
+	if !ok {
+		t.Fatalf("a.firstMember() returns false; want true")
+	}
+	if i != 64 {
+		t.Fatalf("a.firstMember() returns %d; want %d", i, 64)
+	}
+	a.clear()
+	_, ok = a.firstMember()
+	if ok {
+		t.Fatalf("a.firstMember returns true; want false")
+	}
+	w := []int{1, 2, 3, 63, 64, 127, 128}
+	a.insert(w...)
+	s := a.slice()
+	if !equalIntSlices(s, w) {
+		t.Fatalf("a.slice() returned %d; want %d", s, w)
+	}
+	u1 := []int{1, 2, 3, 63, 64, 65, 127, 128, 129}
+	u2 := []int{65, 66, 230, 16500}
+	v := []int{65}
+	var a1, a2, b xset
+	a1.insert(u1...)
+	a2.insert(u2...)
+	a2str := a2.String()
+	const wa2str = "{65, 66, 230, 16500}"
+	if a2str != wa2str {
+		t.Fatalf("a2.String() returned %q; want %q", a2str, wa2str)
+	}
+	b.intersect(&a1, &a2)
+	if len(b) != 2 {
+		t.Fatalf("len(b) is %d; want %d", len(b), 2)
+	}
+	u := b.slice()
+	if !equalIntSlices(u, v) {
+		t.Fatalf("b.intersect(&a1,&a2) is %d; want %d", u, v)
+	}
+	k, ok := a2.memberAfter(230)
+	if !ok {
+		t.Fatalf("a2.memberAfter(%d) returned false; want true", 230)
+	}
+	if k != 16500 {
+		t.Fatalf("a2.memberAfter(%d) returned %d; want %d",
+			230, k, 16500)
+	}
+	_, ok = a2.memberAfter(16500)
+	if ok {
+		t.Fatalf("a2.memberAfter(%d) returned true; want false", 16500)
 	}
 }

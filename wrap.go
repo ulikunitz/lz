f88f6db3848a728717f73f7380e7c14c8f44f944
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2021-12-20 17:56:51 +0100

    Add WrapShrinkSize
    Allow the user to set the shrink size for the WrappedSequencer.
    Otherwise it a default is computed.

--- wrap.go
+++ wrap.go
@@ -6,18 +6,53 @@ import (
 	"reflect"
 )
 
+// WrapShrinkSize produces a WrappedSequencer using a custom shrinkSize.
+func WrapShrinkSize(r io.Reader, seq Sequencer, shrinkSize int) (s *WrappedSequencer, err error) {
+	w := seq.WindowPtr()
+	if shrinkSize < 0 {
+		return nil, errors.New("lz: shrinkSize must be >= 0")
+	}
+	if shrinkSize >= w.size {
+		return nil, errors.New(
+			"lz: shrinkSize must be less than the window size")
+	}
+	return &WrappedSequencer{
+			r:          r,
+			s:          seq,
+			w:          seq.WindowPtr(),
+			shrinkSize: shrinkSize},
+		nil
+}
+
 // Wrap combines a reader and a Sequencer and makes a Sequencer. The user
 // doesn't need to take care of filling the Sequencer with additional data. The
 // returned sequencer returns EOF if no further data is available.
+//
+// Wrap chooses the minimum of 32 kbyte or half of the window size as shrink
+// size.
 func Wrap(r io.Reader, seq Sequencer) *WrappedSequencer {
-	return &WrappedSequencer{r: r, s: seq}
+	const defaultShrinkSize = 32 * 1024
+	w := seq.WindowPtr()
+	var n int
+	if w.size > 2*defaultShrinkSize {
+		n = defaultShrinkSize
+	} else {
+		n = w.size / 2
+	}
+	s, err := WrapShrinkSize(r, seq, n)
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
 
 // WrappedSequencer is returned by the Wrap function. It provides the Sequence
 // method and reads the data required automatically from the stored reader.
 type WrappedSequencer struct {
-	r io.Reader
-	s Sequencer
+	r          io.Reader
+	s          Sequencer
+	w          *Window
+	shrinkSize int
 }
 
 type memSizer interface {
@@ -31,21 +66,15 @@ func (s *WrappedSequencer) MemSize() uintptr {
 	return n
 }
 
-func requestBuffer(s Sequencer, w *Window, blockSize int) int {
-	r := blockSize - w.Buffered()
+func (s *WrappedSequencer) requestBuffer(blockSize int) int {
+	r := blockSize - s.w.Buffered()
 	if r <= 0 {
 		return 0
 	}
-	if w.Available() < r {
-		var ws int
-		if w.size < 64<<10 {
-			ws = w.size / 2
-		} else {
-			ws = 32 << 10
-		}
-		s.Shrink(ws)
+	if s.w.Available() < r {
+		s.s.Shrink(s.shrinkSize)
 	}
-	return w.Available()
+	return s.w.Available()
 }
 
 // Sequence creates a block of sequences but reads the required data from the
@@ -55,16 +84,22 @@ func (s *WrappedSequencer) Sequence(blk *Block, blockSize int, flags int) (n int
 	if blockSize < 1 {
 		return 0, errors.New("lz: blockSize must be >= 1")
 	}
-	w := s.s.WindowPtr()
-	if r := requestBuffer(s.s, w, blockSize); r > 0 {
-		_, err = w.ReadFrom(s.r)
+	if r := s.requestBuffer(blockSize); r > 0 {
+		_, err = s.w.ReadFrom(s.r)
 	}
 	var serr error
 	n, serr = s.s.Sequence(blk, blockSize, flags)
-	if serr == ErrEmptyBuffer && err == nil {
-		serr = io.EOF
+	if serr != nil {
+		if serr == ErrEmptyBuffer && err == nil {
+			err = io.EOF
+		} else {
+			err = serr
+		}
 	}
-	return n, serr
+	if err == ErrFullBuffer {
+		err = nil
+	}
+	return n, err
 }
 
 // Reset puts the WrappedSequencer in its initial state and changes the wrapped

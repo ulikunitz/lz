78a2ef2bf4ee9cb05b4c20a675f6ea5a63d2438e
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2022-12-27 11:00:36 +0100

    Add fuzzing for the hash sequencer
    The commit adds a simple fuzzing function for the hash sequencer. It
    works well.

--- hs_test.go
+++ hs_test.go
@@ -9,6 +9,9 @@ import (
 	"os"
 	"strings"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 func TestHashSequencerSimple(t *testing.T) {
@@ -82,6 +85,57 @@ func TestHashSequencerSimple(t *testing.T) {
 		t.Fatalf("uncompressed string %q; want %q", g, str)
 	}
 	t.Logf("g: %q", g)
+}
+
+func FuzzHashSequencer(f *testing.F) {
+	f.Add(3, 5, []byte("=====foofoobarfoobar bartender===="))
+	f.Fuzz(func(t *testing.T, inputLen int, hashBits int, p []byte) {
+		cfg := HSConfig{
+			SBConfig: SBConfig{
+				WindowSize: 1024,
+				BlockSize:  512,
+			},
+			InputLen: 3,
+		}
+		cfg.ApplyDefaults()
+		if err := cfg.Verify(); err != nil {
+			t.Skip()
+		}
+
+		seq, err := cfg.NewSequencer()
+		if err != nil {
+			t.Fatalf("cfg.NewSequencer() error %s", err)
+		}
+		s := Wrap(bytes.NewReader(p), seq)
+
+		var buffer bytes.Buffer
+		var decoder Decoder
+		err = decoder.Init(&buffer, DConfig{WindowSize: cfg.WindowSize})
+		if err != nil {
+			t.Fatalf("decoder.Init error %s", err)
+		}
+
+		var blk Block
+		for {
+			if _, err := s.Sequence(&blk, 0); err != nil {
+				if err == io.EOF {
+					break
+				}
+				t.Fatalf("s.Sequence error %s", err)
+			}
+			if _, _, _, err := decoder.WriteBlock(blk); err != nil {
+				t.Fatalf("decoder.WriteBlock error %s", err)
+			}
+		}
+		if err := decoder.Flush(); err != nil {
+			t.Fatalf("decoder.Flush error %s", err)
+		}
+
+		q := buffer.Bytes()
+		if diff := cmp.Diff(p, q, cmpopts.EquateEmpty()); diff != "" {
+			t.Fatalf("decoded mismatch (+got -want):\n%s", diff)
+		}
+	})
 }
 
 func TestWrapOldHashSequencer(t *testing.T) {

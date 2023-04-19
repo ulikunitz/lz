633c1bc04a920f56a4d40d438a7c55ebb18e5c76
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2023-04-19 20:52:07 +0200

    Implement doubleHashFinder
    The doubleHashFinder will be used by the Double Hash Sequencer and the
    Backward Double Hash Sequencer.

--- hash.go
+++ hash.go
@@ -122,7 +122,7 @@ func (cfg *HashConfig) NewMatchFinder() (mf MatchFinder, err error) {
 		return nil, err
 	}
 	f := new(hashFinder)
-	f.hash.init(cfg.InputLen, cfg.HashBits)
+	err = f.hash.init(cfg.InputLen, cfg.HashBits)
 	return f, err
 }
 
@@ -195,5 +195,134 @@ func (f *hashFinder) AppendMatchOffsets(m []uint32, i int) []uint32 {
 	}
 	e.pos = uint32(i)
 	e.value = y
+	return m
+}
+
+type DHConfig struct {
+	h1cfg HashConfig
+	h2cfg HashConfig
+}
+
+func (cfg *DHConfig) ApplyDefaults() {
+	cfg.h1cfg.ApplyDefaults()
+	cfg.h2cfg.ApplyDefaults()
+}
+
+func (cfg *DHConfig) Verify() error {
+	var err error
+	if err = cfg.h1cfg.Verify(); err != nil {
+		return err
+	}
+	if err = cfg.h2cfg.Verify(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (cfg *DHConfig) NewMatchFinder() (mf MatchFinder, err error) {
+	cfg.ApplyDefaults()
+	if err = cfg.Verify(); err != nil {
+		return nil, err
+	}
+	f := new(doubleHashFinder)
+	err = f.h1.init(cfg.h1cfg.InputLen, cfg.h1cfg.HashBits)
+	if err != nil {
+		return nil, err
+	}
+	err = f.h2.init(cfg.h2cfg.InputLen, cfg.h2cfg.HashBits)
+	if err != nil {
+		return nil, err
+	}
+	return f, nil
+}
+
+type doubleHashFinder struct {
+	h1    hash
+	h2    hash
+	data  []byte
+	_data []byte
+}
+
+func (f *doubleHashFinder) Update(p []byte, delta int) {
+	switch {
+	case delta < 0:
+		f.h1.reset()
+		f.h2.reset()
+	case delta > 0:
+		f.h1.shiftOffsets(uint32(delta))
+		f.h2.shiftOffsets(uint32(delta))
+	}
+	if len(p) == 0 {
+		f.h1.reset()
+		f.h2.reset()
+		f.data = f.data[:0]
+		return
+	}
+	if len(p)+7 > cap(p) {
+		if len(p)+7 > cap(f.data) {
+			f.data = make([]byte, len(p), len(p)+7)
+		} else {
+			f.data = f.data[:len(p)]
+		}
+		copy(f.data, p)
+	} else {
+		f.data = p
+	}
+	f._data = f.data[:len(p)+7]
+}
+
+// ProcessSegment adds the hashes between position a and b into the hash.
+func (f *doubleHashFinder) ProcessSegment(a, b int) {
+	if a < 0 {
+		a = 0
+	}
+	_p := f._data
+	h1, h2 := &f.h1, &f.h2
+	if h1.inputLen > h2.inputLen {
+		h1, h2 = h2, h1
+	}
+
+	b1, c1 := b, len(f.data)-h1.inputLen+1
+	if c1 < b1 {
+		b1 = c1
+	}
+	b2, c2 := b, len(f.data)-h2.inputLen+1
+	if c2 < b2 {
+		b2 = c2
+	}
+	for i := a; i < b2; i++ {
+		x := _getLE64(_p[i:])
+		e := hashEntry{
+			pos:   uint32(i),
+			value: uint32(x),
+		}
+		h1.table[hashValue(x&h1.mask, h1.shift)] = e
+		h2.table[hashValue(x&h2.mask, h2.shift)] = e
+	}
+	for i := b2; i < b1; i++ {
+		x := _getLE64(_p[i:])
+		h1.table[hashValue(x&h1.mask, h1.shift)] = hashEntry{
+			pos:   uint32(i),
+			value: uint32(x),
+		}
+	}
+}
+
+// AppendMatchOffsets extracts a single offset from the hash table and writes
+// the hash for the current position into the hash.
+func (f *doubleHashFinder) AppendMatchOffsets(m []uint32, i int) []uint32 {
+	x := _getLE64(f._data[i:])
+	y := uint32(x)
+	t := hashEntry{pos: uint32(i), value: uint32(y)}
+	e := &f.h1.table[hashValue(x&f.h1.mask, f.h1.shift)]
+	if e.value == y {
+		m = append(m, e.pos)
+	}
+	*e = t
+	e = &f.h2.table[hashValue(x&f.h2.mask, f.h2.shift)]
+	if e.value == y {
+		m = append(m, e.pos)
+	}
+	*e = t
 	return m
 }

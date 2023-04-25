57649c8ec3457ee93b84fee403184eec93e993d2
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2023-04-25 23:30:52 +0200

    lz: first files of the next lz
    We are implementing a new lz interface. These are the first files for it.

---
+++ lz.go
@@ -0,0 +1,218 @@
+// Package lz supports encoding and decoding of LZ77 sequences. A sequence, as
+// described in the [Zstandard specification], consists of a literal copy
+// command followed by a match copy command. The literal copy command is
+// described by the length in literal bytes to be copied and the match command
+// consists of the distance of the match to copy and the length of the match in
+// bytes.
+//
+// A [Sequencer] is an encoder that converts a byte stream into blocks of
+// sequences. A [Decoder] converts the block of sequences into the original
+// decompressed byte stream. We provide a Sequencer interface only supporting
+// the Sequence interface.
+//
+// The actual basic Sequencer provided by the package support the SeqBuffer
+// interface, which has methods for writing and reading from the buffer. A pure
+// Sequencer is provided by the [Wrap function.
+//
+// The module provides multiple sequencer implementations that provide different
+// combinations of encoding speed  and compression ratios. Usually a slower
+// sequencer will generate a better compression ratio.
+//
+// The [Decoder] slides the decompression window through a larger buffer
+// implemented by [DecBuffer].
+//
+// The library supports the implementation of Sequencers outside the package
+// that can then be used by real compressors as provided by the
+// [github.com/ulikunitz/xz] module.
+//
+// [Zstandard specification]: https://github.com/facebook/zstd/blob/dev/doc/zstd_compression_format.md
+package lz
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"reflect"
+)
+
+// Kilobytes and Megabyte defined as the more precise kibibyte and mebibyte.
+const (
+	kiB = 1 << 10
+	miB = 1 << 20
+)
+
+// Seq represents a single Lempel-Ziv 77 Sequence describing a match,
+// consisting of the offset, the length of the match and the number of
+// literals preceding the match. The Aux field can be used on upper
+// layers to store additional information.
+type Seq struct {
+	LitLen   uint32
+	MatchLen uint32
+	Offset   uint32
+	Aux      uint32
+}
+
+// Block stores sequences and literals. Note that the sequences stores in the
+// Sequences slice might not consume the whole Literals slice. They must be
+// added to the decoded text after all the sequences have been decoded and their
+// content added to the decoder buffer.
+type Block struct {
+	Sequences []Seq
+	Literals  []byte
+}
+
+// Flags for the sequence function stored in the block structure.
+const (
+	// NoTrailingLiterals tells a sequencer that trailing literals don't
+	// need to be included in the block.
+	NoTrailingLiterals = 1 << iota
+)
+
+// ErrEmptyBuffer indicates that no more data is available in the buffer. It
+// will be returned by the Sequence method of  [Sequencer].
+var ErrEmptyBuffer = errors.New("lz: no more data in buffer")
+
+// ErrFullBuffer indicates that the buffer is full. It will be returned by the
+// Write and ReadFrom methods of the [Sequencer].
+var ErrFullBuffer = errors.New("lz: buffer is full")
+
+// Sequencer provides the basic interface of a Sequencer. It provides the
+// functions provided by SeqBuffer.
+type Sequencer interface {
+	Sequence(blk *Block, flags int) (n int, err error)
+	Reset(data []byte)
+	Shrink() int
+	SeqConfig() SeqConfig
+	BufferConfig() BufConfig
+	Write(p []byte) (n int, err error)
+	ReadFrom(r io.Reader) (n int64, err error)
+	ReadAt(p []byte, off int64) (n int, err error)
+	ByteAt(off int64) (c byte, err error)
+}
+
+// BufConfig describes the various sizes relevant for the buffer. Note that
+// ShrinkSize should be significantly smaller than BufferSize at most 50%. The
+// WindowSize is independent of the BufferSize, but usually the BufferSize
+// should be larger or equal the WindowSize. A typical BlockSize for instance
+// for the ZStandard compression is 128 kByte and limits the largest match len.
+type BufConfig struct {
+	ShrinkSize int
+	BufferSize int
+
+	WindowSize int
+	BlockSize  int
+}
+
+func iVal(v reflect.Value, name string) int {
+	return int(v.FieldByName(name).Int())
+}
+
+// bufferConfig reads the BufConfig from the sequencer configuration.
+func bufferConfig(x SeqConfig) BufConfig {
+	v := reflect.Indirect(reflect.ValueOf(x))
+	bc := BufConfig{
+		ShrinkSize: iVal(v, "ShrinkSize"),
+		BufferSize: iVal(v, "BufferSize"),
+		WindowSize: iVal(v, "WindowSize"),
+		BlockSize:  iVal(v, "BlockSize"),
+	}
+	return bc
+}
+
+func setIVal(v reflect.Value, name string, i int) {
+	v.FieldByName(name).SetInt(int64(i))
+}
+
+func setBufferConfig(x SeqConfig, bc BufConfig) {
+	v := reflect.Indirect(reflect.ValueOf(x))
+	setIVal(v, "ShrinkSize", bc.ShrinkSize)
+	setIVal(v, "BufferSize", bc.BufferSize)
+	setIVal(v, "WindowSize", bc.WindowSize)
+	setIVal(v, "BlockSize", bc.BlockSize)
+}
+
+// SeqConfig generates  new sequencer instances. Note that the sequencer doesn't
+// use ShrinkSize and BufferSize directly but we added it here, so it can be
+// used for the WriteSequencer which provides a WriteCloser interface.
+type SeqConfig interface {
+	NewSequence() (s Sequencer, err error)
+	SetDefaults()
+	Verify() error
+}
+
+// Methods to the types defined above.
+
+// Len returns the complete length of the sequence.
+func (s Seq) Len() int64 {
+	return int64(s.MatchLen) + int64(s.LitLen)
+}
+
+// Len computes the length of the block in bytes. It assumes that the sum of the
+// literal lengths in the sequences doesn't exceed that length of the Literals
+// byte slice.
+func (b *Block) Len() int64 {
+	n := int64(len(b.Literals))
+	for _, s := range b.Sequences {
+		n += int64(s.MatchLen)
+	}
+	return n
+}
+
+// Verify checks the buffer configuration. Note that window size and block size
+// are independent of the rest of the other sizes only the shrink size must be
+// less than the buffer size.
+func (cfg *BufConfig) Verify() error {
+	// We are taking care of the margin for tha hash sequencers.
+	maxSize := int64(maxUint32) - 7
+	if int64(maxInt) < maxSize {
+		maxSize = maxInt - 7
+	}
+	if !(1 <= cfg.BufferSize && int64(cfg.BufferSize) <= maxSize) {
+		return fmt.Errorf("lz.BufferConfig.Verify: BufferSize=%d out of range [%d,%d]",
+			cfg.BufferSize, 1, maxSize)
+	}
+	if !(0 <= cfg.ShrinkSize && cfg.ShrinkSize <= cfg.BufferSize) {
+		return fmt.Errorf("lz.BufferConfig.Verify: ShrinkSize=%d out of range [0,BufferSize=%d]",
+			cfg.ShrinkSize, cfg.BufferSize)
+	}
+	if !(0 <= cfg.WindowSize && int64(cfg.WindowSize) <= maxSize) {
+		return fmt.Errorf("lz.BufferConfig.Verify: WindowSize=%d out of range [%d,%d]",
+			cfg.WindowSize, 0, maxSize)
+	}
+	if !(1 <= cfg.BlockSize && int64(cfg.BlockSize) <= maxSize) {
+		return fmt.Errorf("lz.BufferConfig.Verify: cfg.BLockSize=%d out of range [%d,%d]",
+			cfg.BlockSize, 1, maxSize)
+	}
+	return nil
+}
+
+// SetDefaults sets the defaults for the various size values. The defaults are
+// given below.
+//
+//	BufferSize:   8 MiB
+//	ShrinkSize:  32 KiB (or half of BufferSize, if it is smaller than 64 KiB)
+//	WindowSize: BufferSize
+//	BlockSize:  128 KiB
+func (cfg *BufConfig) SetDefaults() {
+	if cfg.WindowSize == 0 {
+		cfg.WindowSize = 8 * miB
+	}
+	if cfg.BufferSize == 0 {
+		cfg.BufferSize = cfg.WindowSize
+	}
+	if cfg.ShrinkSize == 0 {
+		if cfg.BufferSize < 64*kiB {
+			cfg.ShrinkSize = cfg.BufferSize >> 1
+		} else {
+			cfg.ShrinkSize = 32 * kiB
+		}
+	}
+	if cfg.BlockSize == 0 {
+		cfg.BlockSize = 128 * kiB
+	}
+}
+
+// BufConfig returns the buffer configuration.
+func (cfg *BufConfig) BufferConfig() BufConfig {
+	return *cfg
+}
---
+++ seqbuffer.go
@@ -0,0 +1,174 @@
+package lz
+
+import (
+	"errors"
+	"fmt"
+	"io"
+)
+
+// SeqBuffer provides a base for Sequencer implementation. Since the package
+// allows implementations outside of the package. All members are public.
+type SeqBuffer struct {
+	data []byte
+	w    int
+
+	off int64
+
+	cfg BufConfig
+}
+
+// BufferConfig returns the configuration of the buffer.
+func (b *SeqBuffer) BufferConfig() BufConfig { return b.cfg }
+
+// Bytes returns the content of the buffer.
+func (b *SeqBuffer) Bytes() []byte { return b.data }
+
+// W returns the position of the window head in the slice returned by
+// [SeqBuffer.Bytes].
+func (b *SeqBuffer) W() int { return b.w }
+
+// SetW sets the new w. It must be larger or equal the current w and less than
+// or equal the length of the buffer data. The method panics if that is not the
+// case.
+func (b *SeqBuffer) SetW(w int) {
+	if !(b.w <= w && w <= len(b.data)) {
+		panic(fmt.Errorf("lz: w=%d outside of range [%d..%d]",
+			w, b.w, len(b.data)))
+	}
+	b.w = w
+}
+
+// Resets the buffer to the the new data. Note that the buffer will try to use p
+// as internal data slice if possible to avoid copying.
+func (b *SeqBuffer) Reset(data []byte) error {
+	if len(data) > b.cfg.BufferSize {
+		return fmt.Errorf("lz: len(data)=%d larger than BufferSize=%d",
+			len(data), b.cfg.BufferSize)
+	}
+
+	b.w = 0
+	b.off = 0
+
+	// Ensure the margin required for the hash sequencers.
+	margin := len(data) + 7
+	if margin > cap(data) {
+		if margin > cap(b.data) {
+			b.data = make([]byte, len(data), margin)
+		} else {
+			b.data = b.data[:len(data)]
+		}
+		copy(b.data, data)
+	} else {
+		b.data = data
+	}
+
+	return nil
+}
+
+// Shrink will move the window head to the shrink size if it is larger. The
+// amount of data removed from the buffer, named delta, will be returned.
+func (b *SeqBuffer) Shrink() int {
+	delta := b.w - b.cfg.ShrinkSize
+	if delta <= 0 {
+		return 0
+	}
+	n := copy(b.data, b.data[delta:])
+	b.data = b.data[:n]
+	b.w = b.cfg.ShrinkSize
+	b.off += int64(delta)
+	return delta
+}
+
+func (b *SeqBuffer) grow(t int) {
+	if t+7 < cap(b.data) {
+		return
+	}
+	if t > b.cfg.BufferSize {
+		t = b.cfg.BufferSize
+	}
+	var c int
+	if t < 512 {
+		c = 1024
+	} else {
+		c = 2 * cap(b.data)
+		if t > c {
+			c = 2 * t
+		}
+	}
+	if c > b.cfg.BufferSize/2 || c < 0 {
+		c = b.cfg.BufferSize + 7
+	}
+	p := b.data
+	b.data = make([]byte, len(b.data), c)
+	copy(b.data, p)
+}
+
+// Write writes data into the buffer. If not the complete p slice can be copied
+// into the buffer, Write will return [ErrFullBuffer].
+func (b *SeqBuffer) Write(p []byte) (n int, err error) {
+	available := b.cfg.BufferSize - len(b.data)
+	if available < len(p) {
+		p = p[:available]
+		err = ErrFullBuffer
+	}
+	n = len(p)
+
+	t := len(b.data) + n
+	if t+7 > cap(b.data) {
+		b.grow(t)
+	}
+	b.data = append(b.data, p...)
+	return n, err
+}
+
+func (b *SeqBuffer) ReadFrom(r io.Reader) (n int64, err error) {
+	const chunkSize = 32 << 10
+	n = int64(len(b.data))
+	for {
+		t := min(len(b.data)+chunkSize, b.cfg.BufferSize)
+		if t+7 > cap(b.data) {
+			b.grow(t)
+		}
+		p := b.data[len(b.data) : cap(b.data)-7]
+		var k int
+		k, err = r.Read(p)
+		b.data = b.data[:len(b.data)+k]
+		if err != nil {
+			break
+		}
+		if len(b.data) >= b.cfg.BufferSize {
+			err = ErrFullBuffer
+			break
+		}
+	}
+	return int64(len(b.data)) - n, err
+}
+
+// Errors returned by [SeqBuffer.ReadAt]
+var (
+	ErrOutOfBuffer = errors.New("lz: offset out of buffer")
+	ErrEndOfBuffer = errors.New("lz: end of buffer")
+)
+
+func (b *SeqBuffer) ReadAt(p []byte, off int64) (n int, err error) {
+	i := off - b.off
+	if !(0 <= i && i < int64(len(b.data))) {
+		return 0, ErrOutOfBuffer
+	}
+	n = copy(p, b.data[i:])
+	if n < len(p) {
+		err = ErrEndOfBuffer
+	}
+	return n, err
+}
+
+func (b *SeqBuffer) ByteAt(off int64) (c byte, err error) {
+	i := off - b.off
+	if !(0 <= i && i <= int64(len(b.data))) {
+		if i == int64(len(b.data)) {
+			return 0, ErrEndOfBuffer
+		}
+		return 0, ErrOutOfBuffer
+	}
+	return b.data[i], nil
+}
